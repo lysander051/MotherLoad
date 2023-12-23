b543bc6852e9a903560f23efa1b488532558a55b
Itasuka <schivre.nicolas@gmail.com>
2023-12-23 16:07:30 +0100

    Theme fin ?

--- app/src/main/java/com/example/motherload/ui/game/shop/AchatFragment.kt
+++ app/src/main/java/com/example/motherload/ui/game/shop/AchatFragment.kt
@@ -1,21 +1,29 @@
 package com.example.motherload.ui.game.shop
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.motherload.R
 
 class AchatFragment : Fragment(){
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val ret = inflater.inflate(R.layout.fragment_item_top_banner, container, false)
-        ret.findViewById<LinearLayout>(R.id.backgroundInventory).setBackgroundResource(R.drawable.background_for_list_title_day)
+        if (resources.configuration.isNightModeActive){
+            ret.findViewById<LinearLayout>(R.id.backgroundInventory).setBackgroundResource(R.drawable.background_for_list_title_night)
+        }
+        else{
+            ret.findViewById<LinearLayout>(R.id.backgroundInventory).setBackgroundResource(R.drawable.background_for_list_title_day)
+        }
 
         return ret
     }
