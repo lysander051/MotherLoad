93cefdbd84b6d5b6d7650cb6455cbc5b70c1d1ef
lysander051 <badet.maxime@gmail.com>
2023-12-24 01:34:12 +0100

    la suite du push partie 5/8

--- app/src/main/java/com/example/motherload/ui/game/home/HomeFragment.kt
+++ app/src/main/java/com/example/motherload/ui/game/home/HomeFragment.kt
@@ -352,6 +352,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun affichageTrou() {
+
         val myGroundOverlay = GroundOverlay2()
         if(didDig && firstdisp) {
             holePosition = playerPosition
